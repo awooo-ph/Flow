ea7c5320c8aac7c2dfb8204a1f2ed0c5a3e51fcf
awooo-ph <32738756+awooo-ph@users.noreply.github.com>
2018-11-13 17:46:59 +0800

    Update FloodSensor/FloodSensor.ino

--- FloodSensor/FloodSensor.ino
+++ FloodSensor/FloodSensor.ino
@@ -1,77 +1,54 @@
 #include "Settings.h"
 #include "Sms.h"
+#include  "WaterLevel.h"
 
 #define VERSION "1.0.0"
-
 #define SIREN_POWER 7
+#define SIGNAL_BARS 4
+#define GSM_RX 11
+#define GSM_TX 12
 
-uint8_t WaterLevel = 0;
+WaterLevel river;
+SmsClass Sms(GSM_RX,GSM_TX);
+
 uint8_t _prevWaterLevel = 0;
-unsigned long _waterLevelChanged = 0;
-unsigned long _lastLevelCheck = 0;
-
-uint8_t WaterPIN[] = {A0,A1,A2,A3,A6};
-uint8_t SirenPIN[] = {4,5,6};
-uint8_t SignalPIN[] = {8,9,10,11};
+uint8_t SirenPIN[3] = { 4,5,6 };
+uint8_t SignalPIN[SIGNAL_BARS] = { 8,9,10,11 };
 
 void OnWaterLevelChanged(uint8_t level)
 {
     digitalWrite(SIREN_POWER, HIGH);
     delay(777);
 
-    for (auto i=0;i<3;i++)
+    for (auto i = 0; i < 3; i++)
     {
         auto pin = SirenPIN[i];
-        if(Settings.Current.SirenLevel[i]==level)
+        if (Settings.Current.SirenLevel[i] == level)
         {
             digitalWrite(SIREN_POWER, LOW);
             delay(777);
-            digitalWrite(pin,LOW);
-        } else
+            digitalWrite(pin, LOW);
+        }
+        else
         {
-            digitalWrite(pin,HIGH);
+            digitalWrite(pin, HIGH);
         }
     }
 
-    if(Settings.Current.NotifyLevel[level])
+    if (Settings.Current.NotifyLevel[level])
     {
         char * msg = ".0";
-        sprintf(msg,".%d",level);
-        for(auto number:Settings.Current.Monitor)
-            Sms.send(msg,number);
-        for(auto number:Settings.Current.NotifyNumbers)
-            Sms.send(Settings.Current.LevelMessage[level],number);
-    }
-}
-
-void GetLevel()
-{
-    if(millis()-_lastLevelCheck<4444) return;
-
-    uint8_t level = 0;
-
-    for (uint8_t i=0; i<4;i++)
-        if(digitalRead(WaterPIN[i])) level = i;    
-
-    
-    if(WaterLevel!=level)
-    {
-        if(millis()-_waterLevelChanged<7777)
-        {
-            WaterLevel = _prevWaterLevel;
-        } else
-        {
-            _waterLevelChanged = millis();
-            _prevWaterLevel = WaterLevel;
-            WaterLevel = level;
-            OnWaterLevelChanged(level);
-        }
+        sprintf(msg, ".%d", level);
+        for (auto number : Settings.Current.Monitor)
+            Sms.send(msg, number);
+        for (auto number : Settings.Current.NotifyNumbers)
+            Sms.send(Settings.Current.LevelMessage[level], number);
     }
 }
 
 void onReceive(char* number, char* message)
 {
-    if(message[0]=='?')
+    if (message[0] == '?')
     {
         Sms.startSend(number);
         Sms.write("!");
@@ -93,26 +70,33 @@ void onReceive(char* number, char* message)
     }
 }
 
-
-
 void setup() {
-    pinMode(SIREN_POWER,OUTPUT);
-    digitalWrite(SIREN_POWER,HIGH);
+    pinMode(SIREN_POWER, OUTPUT);
+    digitalWrite(SIREN_POWER, HIGH);
 
-    for (auto i : WaterPIN) pinMode(i, INPUT_PULLUP);
+    river.onLevelChange(OnWaterLevelChanged);
+    river.init(A0, A1, A2, A3, A6);
+
     for (auto i : SirenPIN)
     {
         pinMode(i, OUTPUT);
-        digitalWrite(i,HIGH);
+        digitalWrite(i, HIGH);
     }
 
     Settings.LoadConfig();
-    Sms.init();
+
     Sms.onReceive(onReceive);
+    Sms.init();
+
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
-    GetLevel();
+    river.update();
     Sms.update();
+
+    // Signal LED
+    for (auto i = 0; i < SIGNAL_BARS; i++)
+        digitalWrite(SignalPIN[i], Sms.getSignal() > i);
+
 }

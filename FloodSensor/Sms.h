f47b668783e63172788ba520af70a3e800ae3708
awooo-ph <32738756+awooo-ph@users.noreply.github.com>
2018-11-13 21:26:47 +0800

    Update FloodSensor/Sms.h

--- FloodSensor/Sms.h
+++ FloodSensor/Sms.h
@@ -4,47 +4,47 @@
 #define _SMS_h
 
 #if defined(ARDUINO) && ARDUINO >= 100
-	#include "arduino.h"
+#include "arduino.h"
 #else
-	#include "WProgram.h"
+#include "WProgram.h"
 #endif
 
 #include <SoftwareSerial.h>
 
 class SmsClass
 {
- private:
+private:
     SoftwareSerial* sms;
     bool _isReady = false;
     bool _isRegistered = false;
     long _lastCREG = 0;
     bool waitOk();
     char * readLine();
-    uint8_t csq=0;
+    uint8_t csq = 0;
     void processCSQ(char command[]);
     void parseData(char * data);
     char * getNumber(const char * data);
-    bool startsWith(const char *pre,const char *str);
+    bool startsWith(const char *pre, const char *str);
     void parseSMS(char* command);
     bool isAdmin(char * number);
-    void (*onReceiveCallback)(char* number, char* message) = nullptr;
-    
- public:
+    void(*onReceiveCallback)(char* number, char* message) = nullptr;
+
+public:
     SmsClass(uint8_t rx, uint8_t tx);
-	void init();
-    bool isReady(){return _isReady;}
-    bool isRegistered(){return _isRegistered;}
-    uint8_t getRSSI(){return csq;}
+    void init();
+    bool isReady() { return _isReady; }
+    bool isRegistered() { return _isRegistered; }
+    uint8_t getRSSI() { return csq; }
     uint8_t getSignal();
     void update();
     void send(char * message, char * number);
-    void onReceive(void (*callback)(char * number,char * message));
+    void onReceive(void(*callback)(char * number, char * message));
     char * getIMEI();
     void startSend(char * number);
     void write(char * message);
     void write(char text);
     void commitSend();
-    
+
 };
 
 #endif

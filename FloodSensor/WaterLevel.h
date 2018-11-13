f0b4f50e6276a2c9d156d846646dcde3afb91fb9
awooo-ph <32738756+awooo-ph@users.noreply.github.com>
2018-11-13 17:46:30 +0800

    Update FloodSensor/WaterLevel.h

---
+++ FloodSensor/WaterLevel.h
@@ -0,0 +1,28 @@
+// WaterLevel.h
+
+#ifndef _WATERLEVEL_h
+#define _WATERLEVEL_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+class WaterLevel
+{
+private:
+    void (*onLevelChangeCallback)(uint8_t level) = nullptr;
+    uint8_t _currentLevel = 0;
+    uint8_t _prevWaterLevel = 0;
+    uint8_t _levelPIN[5] = { A0,A1,A2,A3,A6 };
+    unsigned long _lastLevelCheck = 0;
+    unsigned long _waterLevelChanged = 0;
+
+ public:
+    void init(uint8_t level1, uint8_t level2, uint8_t level3, uint8_t level4, uint8_t level5);
+    void onLevelChange(void (*onLevelChangeCallback)(uint8_t level));
+    void update();
+};
+
+#endif

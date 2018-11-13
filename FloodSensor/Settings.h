abe76633135a4dbd7b2adc62e103e7089e09bd16
awooo-ph <32738756+awooo-ph@users.noreply.github.com>
2018-11-13 21:31:19 +0800

    Update FloodSensor/Settings.h

--- FloodSensor/Settings.h
+++ FloodSensor/Settings.h
@@ -4,35 +4,36 @@
 #define _SETTINGS_h
 
 #if defined(ARDUINO) && ARDUINO >= 100
-	#include "arduino.h"
+#include "arduino.h"
 #else
-	#include "WProgram.h"
+#include "WProgram.h"
 #endif
 
 #include <EEPROM.h>
+#include <avr/pgmspace.h>
 
 struct Config
 {
-	byte SirenLevel[3] = {1,2,3};
-    byte NotifyLevel[5] = {true,true,true,true,true};
+    byte SirenLevel[3] = { 1,2,3 };
+    byte NotifyLevel[5] = { true,true,true,true,true };
     char * Monitor[4];
     char * NotifyNumbers[7];
     char * LevelMessage[5];
-    char * SensorName = "SENSOR";
-	unsigned int checksum;
+    char * SensorName;
+    unsigned int checksum;
 };
 
 class SettingsClass
 {
 private:
-	
 
- public:
-	 void SaveConfig();
-	 void ResetConfig();
-	 void LoadConfig();
 
-	 Config Current;
+public:
+    void SaveConfig();
+    void ResetConfig();
+    void LoadConfig();
+
+    Config Current;
 };
 
 extern SettingsClass Settings;

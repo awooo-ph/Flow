dc5624e820b71039ee844a0c87b2cdded3beb824
awooo-ph <32738756+awooo-ph@users.noreply.github.com>
2018-11-15 05:46:56 +0800

    added LCD display

---
+++ FloodSensor/Display.cpp
@@ -0,0 +1,159 @@
+// 
+// 
+// 
+
+#include "Display.h"
+
+
+
+DisplayClass::DisplayClass(uint8_t address)
+{
+    lcd = new LiquidCrystal_I2C(address,16,2);
+}
+
+void DisplayClass::showWelcome(char * version)
+{
+    lcd->begin();
+    lcd->setCursor(0,0);
+    lcd->print(F("SFC - GUIHULNGAN"));
+    lcd->setCursor(0,1);
+    lcd->print(F("BSIM  BATCH 2019"));
+    delay(4444);
+    lcd->setCursor(0,0);
+    lcd->print(F("Presents F.M.W.S"));
+    lcd->setCursor(0,1);
+    lcd->print(F("Version  "));
+    lcd->print(version);
+    delay(4444);
+    lcd->setCursor(0,0);
+    lcd->print(F("FLOOD MONITORING"));
+    lcd->setCursor(0,1);
+    lcd->print(F("& WARNING SYSTEM"));
+    delay(4444);
+}
+
+void DisplayClass::showLoading()
+{
+    lcd->setCursor(0,0);
+    lcd->print(F("TURNING MODEM ON"));
+    lcd->setCursor(0,1);
+    lcd->print(F(" PLEASE WAIT... "));
+    delay(1111);
+}
+
+void DisplayClass::init()
+{
+    if(_initialized) return;
+    _initialized = true;
+    lcd->clear();
+
+    lcd->createChar(0,antenna);  
+    lcd->createChar(1,blank);
+    lcd->createChar(2,blank);
+    lcd->createChar(3,water_level);
+    lcd->createChar(4,water_level_2);
+    lcd->createChar(5,blank);
+    lcd->createChar(6,level0);
+
+    lcd->setCursor(0, 0);
+    lcd->write(0);
+
+    lcd->setCursor(1, 0);
+    lcd->write(1);
+    lcd->setCursor(2, 0);
+    lcd->write(2);
+    lcd->setCursor(15, 0);
+    lcd->write(3);
+
+    lcd->setCursor(0, 1);
+    lcd->print(F("-------"));
+    lcd->setCursor(15, 1);
+    lcd->write(4);
+
+    lcd->setCursor(14, 0);
+    lcd->write(5);
+    lcd->setCursor(14, 1);
+    lcd->write(6);
+
+    setLevel(0);
+    setSignal(0);
+}
+
+void DisplayClass::setSignal(int signal)
+{
+    switch (signal)
+    {
+    case -1:
+        lcd->createChar(1, no_signal);
+        lcd->createChar(2, no_signal);
+        break;
+    case 0:
+        lcd->createChar(1, blank);
+        lcd->createChar(2, blank);
+        break;
+    case 1:
+        lcd->createChar(1, bars_1);
+        lcd->createChar(2, blank);
+        break;
+    case 2:
+        lcd->createChar(1, bars_2);
+        lcd->createChar(2, blank);
+        break;
+    case 3:
+        lcd->createChar(1, bars_2);
+        lcd->createChar(2, bars_3);
+        break;
+    case 4:
+        lcd->createChar(1, bars_2);
+        lcd->createChar(2, bars_4);
+        break;
+    }
+}
+
+void DisplayClass::setLevel(uint8_t level)
+{
+    lcd->setCursor(5, 0);
+    lcd->print(F(" LEVEL "));
+    lcd->print(level);
+    switch (level)
+    {
+    case 0:
+        lcd->createChar(5,blank);
+        lcd->createChar(6,level0);
+        break;
+        case 1:
+        lcd->createChar(5,blank);
+        lcd->createChar(6,level1);
+        break;
+        case 2:
+        lcd->createChar(5,blank);
+        lcd->createChar(6,level2);
+        break;
+        case 3:
+        lcd->createChar(5,level3_U);
+        lcd->createChar(6,level3_L);
+        break;
+        case 4:
+        lcd->createChar(5,level4);
+        lcd->createChar(6,level3_L);
+        break;
+        case 5:
+        lcd->createChar(5,level5);
+        lcd->createChar(6,level3_L);
+        break;
+    }
+    
+}
+
+void DisplayClass::setDescription(char* desc)
+{
+    lcd->setCursor(0, 1);
+    lcd->print(F("              "));
+    for(auto i=0;i<strlen(desc);i++)
+    {
+        if(i==14) return;
+        lcd->setCursor(i, 1);
+        lcd->print(desc[i]);
+    }
+    
+}
---
+++ FloodSensor/Display.h
@@ -0,0 +1,61 @@
+// Display.h
+
+#ifndef _DISPLAY_h
+#define _DISPLAY_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+class DisplayClass
+{
+private:
+    bool _initialized = false;
+    LiquidCrystal_I2C * lcd;
+    byte water_level[8] = { B11111,  B10001,  B10001,  B10111,  B10001,  B10001,  B10111,  B10001 };
+    byte water_level_2[8] = { B10001,  B10111,  B10001,  B10001,  B10111,  B10001,  B10001,  B11111 };
+    byte antenna[8] = { B11111,  B10101,  B01110,  B00100,  B00100,  B00100,  B00100,  B00100 };
+    byte bars_1[8] = { B00000,  B00000,  B00000,  B00000,  B00000,  B00000,  B11000,  B11000 };
+    byte bars_2[8] = { B00000,  B00000,  B00000,  B00000,  B00011,  B00011,  B11011,  B11011 };
+    byte bars_3[8] = { B00000,  B00000,  B11000,  B11000,  B11000,  B11000,  B11000,  B11000 };
+    byte bars_4[8] = { B00011,  B00011,  B11011,  B11011,  B11011,  B11011,  B11011,  B11011 };
+    byte level0[8] = { B00000,  B00000,  B00000,  B00000,  B00000,  B00000,  B00000,  B11111 };
+    byte level1[8] = { B00000,  B00000,  B00000,  B00000,  B00111,  B00111,  B00111,  B11111 };
+    byte level2[8] = { B00000,  B00111,  B00111,  B00111,  B00111,  B00111,  B00111,  B11111 };
+    byte level3_L[8] = { B00111,  B00111,  B00111,  B00111,  B00111,  B00111,  B00111,  B11111 };
+    byte level3_U[8] = { B00000,  B00000,  B00000,  B00000,  B00000,  B00000,  B00111,  B00111 };
+    byte level4[8] = { B00000,  B00000,  B00000,  B00111,  B00111,  B00111,  B00111,  B00111 };
+    byte level5[8] = { B00111,  B00111,  B00111,  B00111,  B00111,  B00111,  B00111,  B00111 };
+    byte blank[8] = {0,0,0,0,0,0,0,0};
+    byte no_signal[8] = {
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x1B
+};
+
+ public:
+    DisplayClass(uint8_t address);
+	void showWelcome(char *);
+    void showLoading();
+    void clear();
+    void init();
+    void showError(char * error);
+    void setSignal(int signal);
+    void setLevel(uint8_t level);
+    void setDescription(char * desc);
+};
+
+extern DisplayClass Display;
+
+#endif
+

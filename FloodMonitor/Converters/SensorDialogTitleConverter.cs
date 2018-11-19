f725250dca831c9c6d245bc785793823ebb7b2b5
awooo-ph <32738756+awooo-ph@users.noreply.github.com>
2018-11-19 18:04:29 +0800

    Create SensorDialogTitleConverter.cs

---
+++ FloodMonitor/Converters/SensorDialogTitleConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloodMonitor.Converters
+{
+    class SensorDialogTitleConverter:ConverterBase
+    {
+        protected override object Convert(object value, Type targetType, object parameter)
+        {
+            if (!(value is long id)) return "";
+            return id == 0 ? "NEW SENSOR" : "EDIT SENSOR";
+        }
+    }
+}

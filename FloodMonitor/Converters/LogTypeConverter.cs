7686b8cb293589edcd5f324fbfb2ddd1c38920f4
awooo-ph <32738756+awooo-ph@users.noreply.github.com>
2018-11-19 18:05:56 +0800

    Create LogTypeConverter.cs

---
+++ FloodMonitor/Converters/LogTypeConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using FloodMonitor.ViewModels;
+using MaterialDesignThemes.Wpf;
+
+namespace FloodMonitor.Converters
+{
+    class LogTypeConverter:ConverterBase
+    {
+        protected override object Convert(object value, Type targetType, object parameter)
+        {
+            if (!(value is ModemLog.LogTypes l)) return PackIconKind.Information;
+            switch (l)
+            {
+                case ModemLog.LogTypes.AtCommand:
+                    return PackIconKind.Console;
+                case ModemLog.LogTypes.Sms:
+                    return PackIconKind.Message;
+                default:
+                    return PackIconKind.Alert;
+            }
+        }
+    }
+}

d9aebe0db9f58165462931055b1694dce75ae5a9
awooo-ph <32738756+awooo-ph@users.noreply.github.com>
2018-11-20 04:36:19 +0800

    Create MainViewModel.cs

---
+++ FloodMonitor/ViewModels/MainViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace FloodMonitor.ViewModels
+{
+    class MainViewModel:ViewModelBase
+    {
+        private MainViewModel(){}
+
+        private static MainViewModel _instance;
+        public static MainViewModel Instance => _instance ?? (_instance = new MainViewModel());
+
+        private bool _ShowSensorSettings;
+
+        public bool ShowSensorSettings
+        {
+            get => _ShowSensorSettings;
+            set
+            {
+                if (value == _ShowSensorSettings) return;
+                _ShowSensorSettings = value;
+                OnPropertyChanged(nameof(ShowSensorSettings));
+            }
+        }
+
+        private ICommand _ShowSensorSettingsCommand;
+
+        public ICommand ShowSensorSettingsCommand =>
+            _ShowSensorSettingsCommand ?? (_ShowSensorSettingsCommand = new DelegateCommand<Sensor>(
+                d =>
+                {
+                    SelectedSensor = d;
+                    ShowSensorSettings = true;
+                }));
+
+        private ICommand _HideSensorSettingsCommand;
+
+        public ICommand HideSensorSettingsCommand =>
+            _HideSensorSettingsCommand ?? (_HideSensorSettingsCommand = new DelegateCommand(
+                d => { ShowSensorSettings = false; }));
+
+        private Sensor _SelectedSensor;
+
+        public Sensor SelectedSensor
+        {
+            get => _SelectedSensor;
+            set
+            {
+                if (value == _SelectedSensor) return;
+                _SelectedSensor = value;
+                OnPropertyChanged(nameof(SelectedSensor));
+            }
+        }
+        
+    }
+}

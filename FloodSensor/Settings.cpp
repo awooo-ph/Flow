ea5d045ff04226220dc2233170f913f74ea206dc
awooo-ph <32738756+awooo-ph@users.noreply.github.com>
2018-11-23 04:02:56 +0800

    Update Settings.cpp

--- FloodSensor/Settings.cpp
+++ FloodSensor/Settings.cpp
@@ -9,15 +9,6 @@
 #define MAXIMUM_SENSOR_LOCATION_LENGTH 74
 #endif
 
-//const char STR_LEVEL_1[]     PROGMEM = "LEVEL 1";
-//const char STR_LEVEL_2[]     PROGMEM = "LEVEL 2";
-//const char STR_LEVEL_3[]     PROGMEM = "LEVEL 3";
-//const char STR_LEVEL_4[]     PROGMEM = "LEVEL 4";
-//const char STR_LEVEL_5[]     PROGMEM = "LEVEL 5";
-//const char STR_SENSOR_NAME[] PROGMEM = "SENSOR";
-
-//const char * const SETTINGS_DEFAULTS[] PROGMEM = {STR_LEVEL_1,STR_LEVEL_2,STR_LEVEL_3,STR_LEVEL_4,STR_LEVEL_5,STR_SENSOR_NAME};
-
 void SettingsClass::LoadConfig()
 {
 	Current = Config();
@@ -37,19 +28,6 @@ void SettingsClass::LoadConfig()
 	/* Now check the data we just read */
 	if (Current.checksum != sum) {
 		Current = Config();
-        for (auto i=0;i<7;i++)
-            Current.NotifyNumbers[i] = new char[15];
-
-        //for (auto i=0;i<4;i++)
-        //    Current.Monitor[i] = new char[15];
-
-        for (auto i=0;i<7;i++)
-            Current.Sensors[i] = new char[15];
-        //for(auto i=0;i<5;i++)
-        //{
-        //    Current.LevelMessage[i] = new char[MAXIMUM_MESSAGE_LENGTH];
-        //    strcpy_P(Current.LevelMessage[i],(char *)pgm_read_word(&(SETTINGS_DEFAULTS[i])));
-        //}
         Current.SensorName = new char[MAXIMUM_SENSOR_NAME_LENGTH];
         Current.Location = new char[MAXIMUM_SENSOR_LOCATION_LENGTH];
         strcpy(Current.SensorName,"SENSOR1");

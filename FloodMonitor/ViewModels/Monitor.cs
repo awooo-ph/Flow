c5ae314be00e745f7a5cc16de66278fb8a2521a6
awooo-ph <32738756+awooo-ph@users.noreply.github.com>
2018-12-02 23:28:41 +0800

    Create Monitor.cs

---
+++ FloodMonitor/ViewModels/Monitor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloodMonitor.ViewModels
+{
+    class Monitor:ModelBase<Monitor>
+    {
+        private string _Number;
+
+        public string Number
+        {
+            get => _Number;
+            set
+            {
+                if (value == _Number) return;
+                _Number = value;
+                OnPropertyChanged(nameof(Number));
+            }
+        }
+
+        private string _Name;
+
+        public string Name
+        {
+            get => _Name;
+            set
+            {
+                if (value == _Name) return;
+                _Name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+
+
+        protected override bool GetIsEmpty()
+        {
+            return false;
+        }
+    }
+}

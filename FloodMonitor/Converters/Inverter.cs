4b86a3c83f66f30ed1d76b36275ea23930ca5bf4
awooo-ph <32738756+awooo-ph@users.noreply.github.com>
2018-12-06 23:29:54 +0800

    Create Inverter.cs

---
+++ FloodMonitor/Converters/Inverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloodMonitor.Converters
+{
+    class Inverter:ConverterBase
+    {
+        protected override object Convert(object value, Type targetType, object parameter)
+        {
+            return !(value is bool b && b);
+        }
+    }
+}

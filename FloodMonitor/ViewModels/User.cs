e05ec059186d2e23c7893ed0b7e52fd5bb45b2f5
awooo-ph <32738756+awooo-ph@users.noreply.github.com>
2018-12-06 22:43:43 +0800

    created users

---
+++ FloodMonitor/ViewModels/User.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloodMonitor.ViewModels
+{
+    public sealed class User:ModelBase<User>
+    {
+        protected override bool GetIsEmpty()
+        {
+            return false;
+        }
+
+        private string _Username;
+
+        public string Username
+        {
+            get => _Username;
+            set
+            {
+                if (value == _Username) return;
+                _Username = value;
+                OnPropertyChanged(nameof(Username));
+            }
+        }
+
+        private string _Password;
+
+        public string Password
+        {
+            get => _Password;
+            set
+            {
+                if (value == _Password) return;
+                _Password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+
+        private string _Fullname;
+
+        public string Fullname
+        {
+            get => _Fullname;
+            set
+            {
+                if (value == _Fullname) return;
+                _Fullname = value;
+                OnPropertyChanged(nameof(Fullname));
+            }
+        }
+
+        private string _Position;
+
+        public string Position
+        {
+            get => _Position;
+            set
+            {
+                if (value == _Position) return;
+                _Position = value;
+                OnPropertyChanged(nameof(Position));
+            }
+        }
+
+        private bool _IsAdmin;
+
+        public bool IsAdmin
+        {
+            get => _IsAdmin;
+            set
+            {
+                if (value == _IsAdmin) return;
+                _IsAdmin = value;
+                OnPropertyChanged(nameof(IsAdmin));
+            }
+        }
+
+
+    }
+}

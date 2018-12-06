f1a7c158f1839a5c31b66340e10fabe7012b54fa
awooo-ph <32738756+awooo-ph@users.noreply.github.com>
2018-12-06 23:30:01 +0800

    Update Monitor.cs

--- FloodMonitor/ViewModels/Monitor.cs
+++ FloodMonitor/ViewModels/Monitor.cs
@@ -9,7 +9,8 @@ namespace FloodMonitor.ViewModels
     class Monitor:ModelBase<Monitor>
     {
         private string _Number;
-
+        [Required]
+        [Unique]
         public string Number
         {
             get => _Number;
@@ -22,7 +23,7 @@ namespace FloodMonitor.ViewModels
         }
 
         private string _Name;
-
+        [Required]
         public string Name
         {
             get => _Name;
@@ -34,10 +35,58 @@ namespace FloodMonitor.ViewModels
             }
         }
 
+        private string _Address;
 
+        public string Address
+        {
+            get => _Address;
+            set
+            {
+                if (value == _Address) return;
+                _Address = value;
+                OnPropertyChanged(nameof(Address));
+            }
+        }
+
+        private string _Position;
+
+        public string Position
+        {
+            get => _Position;
+            set
+            {
+                if (value == _Position) return;
+                _Position = value;
+                OnPropertyChanged(nameof(Position));
+            }
+        }
+
+        private string _Remarks;
+
+        public string Remarks
+        {
+            get => _Remarks;
+            set
+            {
+                if (value == _Remarks) return;
+                _Remarks = value;
+                OnPropertyChanged(nameof(Remarks));
+            }
+        }
+
+        protected override string GetErrorInfo(string prop)
+        {
+            if (prop == nameof(Number))
+            {
+                if (!Number.IsCellNumber()) return "INVALID NUMBER";
+            }
+            return base.GetErrorInfo(prop);
+        }
 
         protected override bool GetIsEmpty()
         {
+            if (string.IsNullOrEmpty(Number)) return true;
+            if (string.IsNullOrEmpty(Name)) return true;
             return false;
         }
     }
